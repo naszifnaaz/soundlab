d2a8a4d044c5b1c135eacd4940b8412f4f830c64
naszifnaaz <naszifnaaz@gmail.com>
2023-12-05 22:03:33 +0530

    server: get-users-route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.model");
 
+// Get all users
 router.get("/all", async (req, res) => {
   try {
-    const users = await User.find();
+    const users = await User.find().lean().exec();
     return res.status(200).send({ users });
   } catch (error) {
     console.log("Something went wrong!");
@@ -12,4 +13,17 @@ router.get("/all", async (req, res) => {
   }
 });
 
-module.exports = router
+// Get as specific user by id
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id }).lean().exec();
+    return res.status(200).send({
+      user,
+    });
+  } catch (error) {
+    console.log("Something went wrong!");
+    return res.status(400).send({ message: error.message });
+  }
+});
+
+module.exports = router;
