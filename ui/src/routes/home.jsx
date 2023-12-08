cb7105d0952481ccabd3e2bca4bc912c1a99faad
naszifnaaz <naszifnaaz@gmail.com>
2023-12-08 13:09:08 +0530

    ui: footer-component

--- ui/src/routes/home.jsx
+++ ui/src/routes/home.jsx
@@ -1,9 +1,11 @@
+import { Footer } from "../components/commons/footer";
 import { NavBar } from "../components/commons/navbar";
 
 export const Home = () => {
   return (
     <h1>
       <NavBar />
+      <Footer />
     </h1>
   );
 };
