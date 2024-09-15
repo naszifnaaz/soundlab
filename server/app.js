b1b21b5d98264f21f1bb7411fb2c0fcb8c876058
naszifnaaz <naszifnaaz@gmail.com>
2024-09-16 02:09:43 +0530

    deploy: serverless backend

--- server/app.js
+++ server/app.js
@@ -2,15 +2,17 @@ const connect = require("./configs/db");
 const app = require("./index");
 
 require("dotenv").config();
-const PORT = process.env.PORT || 8080;
 
-// Running Express Server
-app.listen(PORT, async () => {
+const startServer = async () => {
   try {
-    await connect();
-    console.log("Server running on port", PORT);
+    await connect(); // Ensure the database connection is established
+    console.log("Database connected");
   } catch (error) {
     console.log("Something went wrong!");
-    console.log(error);
+    console.error(error);
   }
-});
+};
+
+startServer();
+
+module.exports = app; // Export the app for Vercel's serverless function to handle
--- server/configs/db.js
+++ server/configs/db.js
@@ -1,11 +1,25 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// Connecting to SoundLab MongoDB cluster
-const connect = () => {
-  return mongoose.connect(
-    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@soundlab-db.0hryuwh.mongodb.net/SoundLab`
-  );
+let isConnected = false; // Global variable to track the connection state
+
+const connect = async () => {
+  if (isConnected) {
+    console.log("Using existing database connection");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@soundlab-db.0hryuwh.mongodb.net/SoundLab`
+    );
+    isConnected = db.connections[0].readyState;
+    console.log("Connected to the database");
+  } catch (error) {
+    console.log("Failed to connect to the database");
+    console.error(error);
+    throw error; // Throw the error so you can handle it in the calling function
+  }
 };
 
 module.exports = connect;
